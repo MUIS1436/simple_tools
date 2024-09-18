fe92dab2fafb5d479b79fd2c276759c746c39d7f
Muhammad Usama Intezar <muhammadusamaintezar@gmail.com>
2024-09-18 12:02:25 +0500

    Added previous local code

---
+++ lib/util/images.dart
@@ -0,0 +1,6 @@
+class Images {
+  static const String mobileRecharge =
+      'assets/images/prepaid_mobile_recharge.webp';
+  static const String bmi = 'assets/images/bmi.png';
+  static const String bmr = 'assets/images/bmr.png';
+}
